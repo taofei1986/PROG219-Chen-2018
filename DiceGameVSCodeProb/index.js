b268168086adfa375e2227584452b53855cc1606
taofeichen@gmail.com <taofeichen@gmail.com>
2018-04-08 12:20:50 +0800

    add information in end game

--- DiceGameVSCodeProb/index.js
+++ DiceGameVSCodeProb/index.js
@@ -1,5 +1,7 @@
 ﻿/**
  * Created by bcuser on 4/4/18.
+ * Finished by Taofei Chen & Doug Cottrill 4/8/18.
+ * For PROG 219, Kurt Friedrich instructor.
  */
 let roundText=document.querySelector('#turnCount');//select by id "turnCount"
 let balanceText=document.querySelector('#balance');//select by id "balance"
@@ -16,7 +18,7 @@ let buttonClicked=()=>{
     statusText.innerHTML="<br/  >";// reset the message to blank;
     let randomNumberOne;//define for first random number
     let randomNumberTwo;//define for first random number
-    let winOrlost;
+    let winOrlost;//define for win or lost number 
 
     let diceShow = setInterval(//change imageOne and imageTwo image every 50ms by random number input,
         (()=>{
@@ -36,41 +38,15 @@ let buttonClicked=()=>{
             roundText.innerHTML=roundNumber;// update round number display in web page
             if(balanceNumber===0){//if lost all your money, game over
                 document.querySelector('#ButtonBet').style.display="none";//hide button
-                statusText.innerHTML+="<br/>You lost all your money. Game over!"//display information for end game
+                statusText.innerHTML+="<br/>You lost all your money. Game over!<br/>Reload page to restart!"//display information for end game
             }      
         }, 1000);
-}
-
-let returnImageLink=(inputNumber)=>{//function for get image link by number
-    switch(inputNumber){
-        case 1: //input number is 1
-            return "images/dice-1.jpg";
-            break;
-        case 2://input number is 2
-            return "images/dice-2.jpg";
-            break;
-        case 3://input number is 3
-            return "images/dice-3.jpg";
-            break;
-        case 4://input number is 4
-            return "images/dice-4.jpg";
-            break;
-        case 5://input number is 5
-            return "images/dice-5.jpg";
-            break;
-        case 6://input number is 6
-            return "images/dice-6.jpg";
-            break;
-        default:
-        console.log("Bad input for returnImageLink");//log notice for bad input
-        return "bad input";
-    }
 }
 
 let checkGameWin=(inputNumberOne,inputNumberTwo)=>{//function for check win or lost, win will get 1, lost will get -1;also update this round win or lost information
     if(inputNumberOne===inputNumberTwo){//when 2 random numbers have the same value, return 1
         statusText.style.color='red';
-        statusText.innerHTML="A pair of "+inputNumberOne+", you win this round.";//update win this round information 
+        statusText.innerHTML="A pair of "+inputNumberOne+"s, you win this round.";//update win this round information 
         return 1;
     }
     else if(inputNumberOne+inputNumberTwo===7){//when sum of the 2 random numbers equals 7, return 1
@@ -89,7 +65,7 @@ let checkGameWin=(inputNumberOne,inputNumberTwo)=>{//function for check win or l
         return -1;
     }
 }
-let getDispDice=(imageUsed)=>{
+let getDispDice=(imageUsed)=>{// function to get a random number between 1 to 6, and display image by pass in Element object.
 	var diceNum = Math.floor(Math.random() * 6) + 1;
 	imageUsed.src = "./images/dice-" + diceNum.toString() + ".jpg";
 	return diceNum;
