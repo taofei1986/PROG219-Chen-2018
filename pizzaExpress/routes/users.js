bdc55495dd36dcee71151c3503e97d7b8b475ecb
taofeichen@gmail.com <taofeichen@gmail.com>
2018-05-29 23:26:56 -0700

    pizzaExpress Login and create user part working

---
+++ pizzaExpress/routes/users.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+
+//bcPizzariaOrder  bcPizzariaUser
+
+/* POST to adduser. */
+router.post('/adduser', function(req, res) {
+  var db = req.db;
+  var collection = db.get('bcPizzariaUser');
+  collection.find({
+    username:req.body.username
+  },(err, docs)=>{
+    if(err){
+        res.send({msg:'server or db error'});
+    }else{
+      if(docs.length==0){
+        collection.insert(req.body, function(err, result){
+          res.send(
+            (err === null) ? { msg: 'create accout success' } : { msg: err }
+          );
+        })
+      }
+      else{
+        res.send({msg:'already have this accout',accoutExsit:true});
+      }
+    }
+  })
+});
+
+/* POST to login. */
+router.post('/login', function(req, res) {
+  var db = req.db;
+  var collection = db.get('bcPizzariaUser');
+  collection.find({
+    username:req.body.username,
+    password:req.body.password
+  },(err, docs)=>{
+    if(err){
+        res.send({msg:'server or db error'});
+    }else{
+      if(docs.length==0){
+        res.send(
+          (err === null) ? { msg: 'username incorrect or password incrorrect!',loginSuccess:false} : { msg: err }
+        );
+      }
+      else{
+        res.send({msg:'login success',loginSuccess:true});
+      }
+    }
+  })
+});
+
+module.exports = router;
